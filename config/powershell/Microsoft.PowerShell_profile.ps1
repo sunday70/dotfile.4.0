b4de4dbaa93af9836e56530289fe9c17a74e8a9f
sunday70 <85828382+sunday70@users.noreply.github.com>
2022-06-13 23:32:16 +0800

    Add files via upload

---
+++ config/powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,16 @@
+oh-my-posh init pwsh --config ~/.config/powershell/craver.omp.json | Invoke-Expression
+
+#  Set-Alias
+Set-Alias co 'cowsay'
+Set-Alias cm 'cmatrix'
+Set-Alias v 'nvim'
+Set-Alias c 'cls'
+Set-Alias ra 'ranger'
+Set-Alias ne 'neofetch'
+Set-Alias f 'fzf'
+Set-Alias h 'htop'
+Set-Alias n 'nano'
+Set-Alias z '7z'
+Set-Alias ca 'caffeinate'
+
+#powershell.exe -nologo
