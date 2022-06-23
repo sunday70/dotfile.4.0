ef7f0622cfe329eaa3610a4b7bdfc80e1f404669
sunday70 <85828382+sunday70@users.noreply.github.com>
2022-06-23 09:51:41 +0800

    Update and rename init.lua to mappings.lua

---
+++ config/nvim/mappings.lua
@@ -0,0 +1,33 @@
+-- 浏览
+maps.n["t"] = { "j", noremap = "false" }
+maps.n["n"] = { "k", noremap = "false" }
+maps.n["s"] = { "l", noremap = "false" }
+maps.n["j"] = { "t", noremap = "false" }
+maps.n["k"] = { "n", noremap = "false" }
+maps.n["l"] = { "s", noremap = "false" }
+maps.n["T"] = { "J", noremap = "false" }
+maps.n["N"] = { "K", noremap = "false" }
+maps.n["S"] = { "L", noremap = "false" }
+maps.n["J"] = { "T", noremap = "false" }
+maps.n["K"] = { "N", noremap = "false" }
+maps.n["L"] = { "S", noremap = "false" }
+
+-- 命令行下
+maps.v["t"] = { "j",  noremap = "false" }
+maps.v["n"] = { "k",  noremap = "false" }
+maps.v["s"] = { "l",  noremap = "false" }
+maps.v["j"] = { "t",  noremap = "false" }
+maps.v["k"] = { "n",  noremap = "false" }
+maps.v["l"] = { "s",  noremap = "false" }
+maps.v["T"] = { "J",  noremap = "false" }
+maps.v["N"] = { "K",  noremap = "false" }
+maps.v["S"] = { "L",  noremap = "false" }
+maps.v["J"] = { "T",  noremap = "false" }
+maps.v["K"] = { "N",  noremap = "false" }
+maps.v["L"] = { "S",  noremap = "false" }
+
+-- 跳跃
+maps.n["("] = { "^", noremap = "false" }
+maps.n[")"] = { "$", noremap = "false" }
+maps.n["^"] = { "(", noremap = "false" }
+maps.n["$"] = { ")", noremap = "false" }
